5a97b3f1f32366a27e710396ecb257b686dcdf57
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-03-03 21:32:06 -0500

    Creating JSON file of all clothing data.

---
+++ src/clothes.js
@@ -0,0 +1,90 @@
+export const clothes = [ //An array of each Shirt's ID, name, and each piece
+  {
+    id: 'outfit1', //For shirts, append _frontarm_lowerhalf, _frontarm_upperhalf, _chest, _backarm_lowerhalf, _backarm_upperhalf to find id of each div. 
+                   //For pants, append _frontleg_upperhalf, _frontleg_lowerhalf, _frontleg_foot, _crotch, _backleg_upperhalf, _backleg_lowerhalf, _backleg_foot to find id of each class.
+                   //For hats, append ??
+    atlas: '02', //Append to atlas to find class.
+    name: 'Advanced BoS' //Show name in textbox.
+  },
+  {
+    id: 'outfit2',
+    atlas: '02',
+    name: 'Expert BoS'
+  },
+  {
+    id: 'outfit3',
+    atlas: '02',
+    name: 'Clergy'
+  },
+  {
+    id: 'outfit4',
+    atlas: '02',
+    name: 'BoS'
+  },
+  {
+    id: 'outfit5',
+    atlas: '02',
+    name: 'Baseball'
+  },
+  {
+    id: 'outfit6',
+    atlas: '02',
+    name: 'Battle armor'
+  },
+  {
+    id: 'outfit8',
+    atlas: '02',
+    name: 'Tenpenny\'s suit'
+  },
+
+
+
+  {
+    id: 'outfit9',
+    atlas: '03',
+    name: 'That\'s a Nice Shirt!'
+  },
+  {
+    id: 'outfit10',
+    atlas: '03',
+    name: 'Sweater Vest with Tie'
+  },
+  {
+    id: 'outfit11',
+    atlas: '03',
+    name: 'Sweater Vest'
+  },
+  {
+    id: 'outfit12',
+    atlas: '03',
+    name: 'Flight Suit'
+  },
+  {
+    id: 'outfit13',
+    atlas: '03',
+    name: 'Autumn\'s uniform'
+  },
+  {
+    id: 'outfit14',
+    atlas: '03',
+    name: 'Combat Armor'
+  },
+  {
+    id: 'outfit15',
+    atlas: '03',
+    name: 'Tunnel Snake\'s outfit'
+  },
+  {
+    id: 'outfit16',
+    atlas: '03',
+    name: 'Ex Con\'s outfit'
+  }
+
+
+  ,
+  {
+    id: 'outfit42',
+    atlas: '07',
+    name: 'Raider Armor'
+  }
+];
