8e85db827e33f3116ebd70c5371ff9f2855efbd5
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-03-07 23:39:34 -0500

    Creating JSON file for male hairstyles.

---
+++ src/malehair.js
@@ -0,0 +1,127 @@
+export const malehair = [ //An array of each Male hairstyle's ID, Atlas file, name, and Hem (if available).
+  {
+    id: 'hair1', //For hairstyles, append #vb_ to find id of div. 
+    atlas: '17', //Append to atlas to find class.
+    name: 'Professor' //Show name in textbox.
+  },
+  {
+    id: 'hair2',
+    atlas: '17',
+    name: '#2'
+  },
+  {
+    id: 'hair3',
+    atlas: '17',
+    name: '#3'
+  },
+  {
+    id: 'hair4',
+    atlas: '17',
+    name: 'Rebel'
+  },
+  {
+    id: 'hair5',
+    atlas: '17',
+    name: 'Urban Ranger'
+  },
+  {
+    id: 'hair6',
+    atlas: '17',
+    name: 'Shaggy Suave'
+  },
+  {
+    id: 'hair7',
+    atlas: '17',
+    name: 'Garage Band'
+  },
+  {
+    id: 'hair8',
+    atlas: '17',
+    name: '#8'
+  },
+  {
+    id: 'hair9',
+    atlas: '17',
+    name: 'Combover'
+  },
+  {
+    id: 'hair10',
+    atlas: '17',
+    name: '#10'
+  },
+  {
+    id: 'hair11',
+    atlas: '17',
+    name: '#11'
+  },
+  {
+    id: 'hair12',
+    atlas: '17',
+    name: '#12'
+  },
+  {
+    id: 'hair13',
+    atlas: '17',
+    name: '#13'
+  },
+  {
+    id: 'hair14',
+    atlas: '17',
+    name: 'Beatnik'
+  },
+  {
+    id: 'hair15',
+    atlas: '17',
+    name: 'Dapper'
+  },
+  {
+    id: 'hair16',
+    atlas: '17',
+    name: '#16'
+  },
+  {
+    id: 'hair17',
+    atlas: '17',
+    name: 'Colonial'
+  },
+  {
+    id: 'hair18',
+    atlas: '17',
+    name: 'Heavyweight'
+  },
+  {
+    id: 'hair19',
+    atlas: '17',
+    name: 'Heavyweight'
+  },
+  {
+    id: 'hair20',
+    atlas: '17',
+    name: '#20'
+  },
+  {
+    id: 'hair21',
+    atlas: '17',
+    name: 'Pompadour'
+  },
+  {
+    id: 'hair22',
+    atlas: '17',
+    name: 'Pompadour'
+  },
+  {
+    id: 'hair23',
+    atlas: '17',
+    name: 'Boot Camp'
+  },
+  {
+    id: 'hair24',
+    atlas: '17',
+    name: 'Bad Hair Day'
+  },
+  {
+    id: 'hair25',
+    atlas: '17',
+    name: 'Casual Kempt'
+  }
+];
