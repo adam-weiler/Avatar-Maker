fe71f8efa6c06bc3ad90be98ccb448dcd8d362c9
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-03-10 23:51:35 -0400

    Updating clothes JSON file.

--- src/clothes.js
+++ src/clothes.js
@@ -1,165 +1,225 @@
-export const clothes = [ //An array of each Shirt's ID, name, and each piece
+export const clothes = [ //An array of each Shirt's ID, Atlas file, name, and Hem (if available).
   {
     id: 'outfit1', //For shirts, append _frontarm_lowerhalf, _frontarm_upperhalf, _chest, _backarm_lowerhalf, _backarm_upperhalf to find id of each div. 
                    //For pants, append _frontleg_upperhalf, _frontleg_lowerhalf, _frontleg_foot, _crotch, _backleg_upperhalf, _backleg_lowerhalf, _backleg_foot to find id of each class.
                    //For hats, append ??
-    atlas: '02', //Append to atlas to find class.
+    atlas: 'atlas02', //Append to atlas to find class.
     name: 'Advanced BoS' //Show name in textbox.
   },
   {
     id: 'outfit2',
-    atlas: '02',
+    atlas: 'atlas02',
     name: 'Expert BoS'
   },
   {
     id: 'outfit3',
-    atlas: '02',
+    atlas: 'atlas02',
     name: 'Clergy',
     hem: 1
   },
   {
     id: 'outfit4',
-    atlas: '02',
+    atlas: 'atlas02',
     name: 'BoS'
   },
   {
     id: 'outfit5',
-    atlas: '02',
+    atlas: 'atlas02',
     name: 'Baseball'
   },
   {
     id: 'outfit6',
-    atlas: '02',
+    atlas: 'atlas02',
     name: 'Battle armor'
   },
   {
     id: 'outfit8',
-    atlas: '02',
+    atlas: 'atlas02',
     name: 'Tenpenny\'s suit'
   },
   {
     id: 'outfit9',
-    atlas: '03',
+    atlas: 'atlas03',
     name: 'That\'s a Nice Shirt!'
   },
   {
     id: 'outfit10',
-    atlas: '03',
+    atlas: 'atlas03',
     name: 'Sweater Vest with Tie'
   },
   {
     id: 'outfit11',
-    atlas: '03',
+    atlas: 'atlas03',
     name: 'Sweater Vest'
   },
   {
     id: 'outfit12',
-    atlas: '03',
+    atlas: 'atlas03',
     name: 'Flight Suit'
   },
   {
     id: 'outfit13',
-    atlas: '03',
+    atlas: 'atlas03',
     name: 'Autumn\'s uniform',
     hem: 1
   },
   {
     id: 'outfit14',
-    atlas: '03',
+    atlas: 'atlas03',
     name: 'Combat Armor'
   },
   {
     id: 'outfit15',
-    atlas: '03',
+    atlas: 'atlas03',
     name: 'Tunnel Snake\'s outfit'
   },
   {
     id: 'outfit16',
-    atlas: '03',
+    atlas: 'atlas03',
     name: 'Ex Con\'s outfit'
   },
   {
     id: 'outfit17',
-    atlas: '04',
+    atlas: 'atlas04',
     name: 'Confessor Cromwell\'s rags',
     hem: 1
   },
   {
     id: 'outfit18',
-    atlas: '04',
+    atlas: 'atlas04',
     name: 'Elf outfit',
     hem: 1
   },
   {
     id: 'outfit19',
-    atlas: '04',
+    atlas: 'atlas04',
     name: 'Motorcyle jacket'
   },
   {
     id: 'outfit20',
-    atlas: '04',
+    atlas: 'atlas04',
     name: 'Business suit'
   },
   {
     id: 'outfit21',
-    atlas: '04',
+    atlas: 'atlas04',
     name: 'Bowling shirt'
   },
   {
     id: 'outfit22',
-    atlas: '04',
+    atlas: 'atlas04',
     name: 'Rad Suspenders'
   },
   {
     id: 'outfit23',
-    atlas: '04',
+    atlas: 'atlas04',
     name: 'Pre-War Intellectual'
   },
   {
     id: 'outfit24',
-    atlas: '04',
+    atlas: 'atlas04',
     name: 'Sci-fi fan outfit'
   },
+
+
+
+  {
+    id: 'outfit25',
+    atlas: 'atlas05',
+    name: 'Handyman jumpsuit'
+  },
+  {
+    id: 'outfit26',
+    atlas: 'atlas05',
+    name: 'Wasteland gear'
+  },
+  {
+    id: 'outfit27',
+    atlas: 'atlas05',
+    name: 'Formal wear'
+  },
+  // {
+  //   id: 'outfit28',
+  //   atlas: 'atlas05',
+  //   name: 'Ghost costume',
+  //   hem: 1
+  // },
+  {
+    id: 'outfit29',
+    atlas: 'atlas05',
+    name: 'Tattered longcoat',
+    hem: 1
+  },
+  {
+    id: 'outfit30',
+    atlas: 'atlas05',
+    name: 'Flight suit'
+  },
+  {
+    id: 'outfit31',
+    atlas: 'atlas05',
+    name: 'Radiation suit'
+  },
+  {
+    id: 'outfit32',
+    atlas: 'atlas05',
+    name: 'Eulogy Jones\' suit'
+  },
+
+
+
   {
     id: 'outfit41',
-    atlas: '07',
+    atlas: 'atlas07',
     name: 'Minutemen uniform',
     hem: 1
   },
   {
     id: 'outfit42',
-    atlas: '07',
+    atlas: 'atlas07',
     name: 'Raider armor'
   },
   {
     id: 'outfit43',
-    atlas: '07',
+    atlas: 'atlas07',
     name: 'T-60 power armor'
   },
   {
     id: 'outfit44',
-    atlas: '07',
+    atlas: 'atlas07',
     name: 'X-01 Mk VI power armor'
   },
   {
     id: 'outfit45',
-    atlas: '07',
+    atlas: 'atlas07',
     name: 'T-45 power armor'
   },
   {
     id: 'outfit46',
-    atlas: '07',
+    atlas: 'atlas07',
     name: 'T-51 power armor'
   },
   {
     id: 'outfit47',
-    atlas: '07',
+    atlas: 'atlas07',
     name: 'Nightwear'
   },
   {
     id: 'outfit48',
-    atlas: '07',
+    atlas: 'atlas07',
     name: 'Pilgrim outfit',
     hem: 1
+  },
+  {
+    id: 'outfit57',
+    atlas: 'mysterious',
+    name: 'Mysterious Stranger',
+    hem: 1
   }
+  //,
+  // {
+  //   id: 'outfit58',
+  //   atlas: 'feral_ghoul',
+  //   name: 'Feral Ghoul',
+  // }
 ];
