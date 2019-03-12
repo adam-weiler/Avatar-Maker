8e899ee2e6fc88612f3cb8d4cd70ab49ab916194
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-03-11 23:34:19 -0400

    Updating hair colours JSON.

---
+++ src/hair_colours.js
@@ -0,0 +1,159 @@
+export const hair_colours = [ //An array of each Colour hairstyle's ID and name.
+  /* The first 21 colours appear in Fallout Shelter. */
+  {
+    id: 'colour1', //For hairstyles, append #vb_ to find id of div. 
+    name: '#1' //Show name in textbox.
+  },
+  {
+    id: 'colour2',
+    name: '#2' 		/* NAME ME!! */
+  },
+  {
+    id: 'colour3',
+    name: 'Brown'    /* Harkness & Mr. Burke from F03. */
+  },
+  {
+    id: 'colour4',
+    name: '#4'    /* NAME ME!! */
+  },
+  {
+    id: 'colour5',
+    name: '#5'    /* NAME ME!! */
+  },
+  {
+    id: 'colour6',
+    name: 'White'    /* Old Longfellow from FO4. */
+  },
+  {
+    id: 'colour7',
+    name: '#7'    /* NAME ME!! */
+  },
+  {
+    id: 'colour8',
+    name: '#8'    /* NAME ME!! */
+  },
+  {
+    id: 'colour9',
+    name: '#9'    /* NAME ME!! */
+  },
+  {
+    id: 'colour10',
+    name: '#10'    /* NAME ME!! */
+  },
+  {
+    id: 'colour11',
+    name: '#11'    /* NAME ME!! */
+  },
+  {
+    id: 'colour12',
+    name: '#12'    /* NAME ME!! */
+  },
+  {
+    id: 'colour13',
+    name: '#13'    /* NAME ME!! */
+  },
+  {
+    id: 'colour14',
+    name: 'Firetruck Red'
+  },
+  {
+    id: 'colour15',
+    name: '#15'    /* NAME ME!! */
+  },
+  {
+    id: 'colour16',
+    name: '#16'    /* NAME ME!! */
+  },
+  {
+    id: 'colour17',
+    name: 'Evergreen'
+  },
+  {
+    id: 'colour18',
+    name: 'Blueberry Hill'
+  },
+  {
+    id: 'colour19',
+    name: 'Russian Violet'
+  },
+  {
+    id: 'colour20',
+    name: 'Your Eminence'
+  },
+  {
+    id: 'colour21',
+    name: 'Artist\'s Purple'
+  },
+
+  /* The following colors are not in-game, but look nice so I included them. */
+  {
+    id: 'colour22',
+    name: 'Nuka Quantum'
+  },
+  {
+    id: 'colour23',
+    name: 'Scarlet'
+  },
+  {
+    id: 'colour24',
+    name: 'Polynesian Blue'
+  },
+  {
+    id: 'colour25',
+    name: 'Bubblegum'
+  },
+  {
+    id: 'colour26',
+    name: 'Geiger Green'
+  },
+  {
+    id: 'colour27',
+    name: 'Chipped Ice'
+  },
+  {
+    id: 'colour28',
+    name: 'Fallout Blonde'
+  }
+]
+
+// Fallout 4 colours:
+// Auburn
+// Bleached
+// Blond
+// Brown
+// Chestnut Brown
+// Cool Brown
+// Dark Brown
+// Deep Brown
+// Deep Red
+// Dirty Blond
+// Ginger
+// Golden Blond
+// Gray
+// Graying
+// Jet Black
+// Light Brown
+// Platnum Blond
+// Red
+// Silver
+// Steel Gray
+// Warm Black
+//     White
+
+// Fallout 3 colours:
+// Bleached
+//     Brown
+// Chocolate
+// Platinum
+// Cornsilk
+// Suede
+// Pecan
+// Auburn
+// Ginger
+// Honey
+// Gold
+// Rosewood
+//     Black
+// Chestnut
+// Steel
+// Champagne
