924097a0de9ff3e32c1d8fef6488fc6329c5cb81
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-03-13 23:46:52 -0400

    Creating JSON file for Male Hats.

---
+++ src/male_hats.js
@@ -0,0 +1,219 @@
+export const male_hats = [ //An array of each Male Hat's ID and name.
+  {
+    id: '0', 
+    atlas: '',
+    name: 'No hat' //Show name in textbox.
+  },
+  {
+    id: '1', //For hat, append ? to find id of div. 
+    atlas: 'atlas17', //Use as find class name.
+    name: 'Metal helmet' /* Metal armor */
+  },
+  {
+    id: '2',
+    atlas: 'atlas17',
+    name: 'Mascot head'
+  },
+  {
+    id: '3',
+    atlas: 'atlas17',
+    name: 'Military helmet' /* Military fatigues */
+  },
+  {
+    id: '4',
+    atlas: 'atlas17',
+    name: 'Professor cap' /* Professor outfit */
+  },
+  {
+    id: '5',
+    atlas: 'atlas17',
+    name: 'Ninja mask' /* Ninja outfit */
+  },
+  {
+    id: '6',
+    atlas: 'atlas17',
+    name: 'Nose glasses' /* Comedian outfit */
+  },
+  {
+    id: '7',
+    atlas: 'atlas17',
+    name: 'T-51 helmet' /* T-51 power armor */
+  },
+  {
+    id: '8',
+    atlas: 'atlas17',
+    name: 'T-60 helmet' /* T-60 power armor */
+  },
+  {
+    id: '9',
+    atlas: 'atlas17',
+    name: 'X-01 Mk VI helmet' /* X-01 Mk VI power armor */
+  },
+  {
+    id: '10',
+    atlas: 'atlas17',
+    name: 'Wasteland darklight cowl' /* Wasteland gear */
+  },
+  {
+    id: '11',
+    atlas: 'atlas17',
+    name: 'Ghost costume' /* Ghost costume */
+  },
+  {
+    id: '12',
+    atlas: 'atlas17',
+    name: 'Shower cap'
+  },
+  {
+    id: '13',
+    atlas: 'atlas17',
+    name: 'Surgery bandages'
+  },
+  {
+    id: '14',
+    atlas: 'atlas17',
+    name: 'Hockey mask' /* Horror fan outfit */
+  },
+  {
+    id: '15',
+    atlas: 'atlas17',
+    name: 'Golden crown' /* Medieval ruler outfit */
+  },
+  {
+    id: '16',
+    atlas: 'atlas17',
+    name: 'Knight helmet' /* Knight armor */
+  },
+  {
+    id: '17',
+    atlas: 'atlas17',
+    name: 'Sailor hat' /* Lifeguard outfit */
+  },
+  {
+    id: '18',
+    atlas: 'atlas17',
+    name: 'Luchador mask' /* Wrestler outfit */
+  },
+  {
+    id: '19',
+    atlas: 'atlas17',
+    name: '???'
+  },
+  {
+    id: '20',
+    atlas: 'atlas17',
+    name: 'Baseball cap' /* Baseball uniform */
+  },
+  {
+    id: '21',
+    atlas: 'atlas17',
+    name: 'Gold tiara' /* Nobility outfit */
+  },
+  {
+    id: '22',
+    atlas: 'atlas17',
+    name: 'Combat armor helmet' /* Combat armor */
+  },
+  {
+    id: '23',
+    atlas: 'atlas17',
+    name: 'Sheriff hat' /* Sheriff's duster */
+  },
+  {
+    id: '24',
+    atlas: 'atlas17',
+    name: 'Elf hat' /* Elf outfit */
+  },
+  {
+    id: '25',
+    atlas: 'atlas17',
+    name: 'Radiation suit helmet' /* Radiation suit */
+  },
+  {
+    id: '26',
+    atlas: 'atlas17',
+    name: 'Vulcan ears' /* Sci-fi fan outfit */
+  },
+  {
+    id: '27',
+    atlas: 'atlas17',
+    name: 'Football helmet' /* Sports fan outfit */
+  },
+  {
+    id: '28',
+    atlas: 'atlas17',
+    name: '???'
+  },
+
+
+  {
+    id: '29',
+    atlas: 'atlas18',
+    name: '???'
+  },
+  {
+    id: '30',
+    atlas: 'atlas18',
+    name: 'Santa hat' /* Santa suit */
+  },
+  {
+    id: '31',
+    atlas: 'atlas18',
+    name: 'Minuteman hat' /* Minuteman uniform */
+  },
+  {
+    id: '32',
+    atlas: 'atlas18',
+    name: 'T-45 helmet' /* T-45 power armor */
+  },
+  {
+    id: '33',
+    atlas: 'atlas18',
+    name: 'Black bowler hat' /* Eulogy Jones' outfit */
+  },
+  {
+    id: '34',
+    atlas: 'atlas18',
+    name: '??? pirat'
+  },
+  {
+    id: '35',
+    atlas: 'atlas18',
+    name: 'Pilgrim hat' /* Pilgrim outfit */
+  },
+  {
+    id: '36',
+    atlas: 'atlas18',
+    name: '???party'
+  },
+  {
+    id: '37',
+    atlas: 'atlas18',
+    name: 'Skeleton mask' /* Skeleton costume */
+  },
+  {
+    id: '38',
+    atlas: 'atlas18',
+    name: 'Swimcap and goggles' /* Swimsuit outfit */
+  },
+  {
+    id: '39',
+    atlas: 'atlas18',
+    name: 'Synth field helmet' /* Synth armor */
+  },
+  {
+    id: '40',
+    atlas: 'atlas18',
+    name: 'Three Dog\'s head wrap' /* Three Dog's outfit */
+  },
+  {
+    id: '41',
+    atlas: 'atlas21',
+    name: 'Top hat' /* Mayor outfit */
+  },
+  {
+    id: '42',
+    atlas: 'atlas21',
+    name: 'Clergy hat' /* Clergy outfit */
+  }
+]
