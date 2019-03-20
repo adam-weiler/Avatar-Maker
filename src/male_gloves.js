3872f6998964b52b18fd563e96edbe9a46391eab
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-03-19 23:36:34 -0400

    Adding Male Gloves json file.

---
+++ src/male_gloves.js
@@ -0,0 +1,69 @@
+export const male_gloves = [ //An array of each Male Glove's ID, Atlas file, and name.
+  {
+    id: '0', //Append either vb_frontarm_hand or vb_backarm_hand to find class.
+    atlas: 'atlas18', //Append to atlas to find class.
+    name: 'No gloves' //Show name in textbox.
+  },
+  {
+    id: '3',
+    atlas: 'atlas18',
+    name: 'Something metal?'
+  },
+  {
+    id: '5',
+    atlas: 'atlas18',
+    name: 'BoS uniform'
+  },
+  {
+    id: '8',
+    atlas: 'atlas18',
+    name: 'Durable gloves' /* Radiation suit, Minuteman uniform. */
+  },
+  {
+    id: '9',
+    atlas: 'atlas18',
+    name: 'Boxing gloves'
+  },
+  {
+    id: '11',
+    atlas: 'atlas18',
+    name: 'Baseball gloves'
+  },
+  {
+    id: '12',
+    atlas: 'atlas18',
+    name: 'Power armor gloves'
+  },
+  {
+    id: '13',
+    atlas: 'atlas18',
+    name: 'Phalanges gloves' /* Skeleton costume. */
+  },
+
+  /* Closed gloves */
+  // {
+  //   id: '1',
+  //   atlas: 'atlas18',
+  //   name: '???'
+  // },
+  // {
+  //   id: '6',
+  //   atlas: 'atlas18',
+  //   name: '???'
+  // },
+  // {
+  //   id: '7',
+  //   atlas: 'atlas18',
+  //   name: '???'
+  // },
+  // {
+  //   id: '10',
+  //   atlas: 'atlas18',
+  //   name: '???'
+  // },
+  // {
+  //   id: '14',
+  //   atlas: 'atlas18',
+  //   name: '???'
+  // }
+]
