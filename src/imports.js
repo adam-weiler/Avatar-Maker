67551f98133810aca70779a26f91faae3ff35b13
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-04-03 23:43:46 -0400

    Creating import file.
    File to include all imports in 1 place.

---
+++ src/imports.js
@@ -0,0 +1,51 @@
+/* All JSON files. */
+/* Male and Female JSONs */
+import { all_races } from './JSON/all_races'; //Contains data about skincolor.
+import { genders } from './JSON/genders'; //Contains data about Genders.
+import { hair_colours } from './JSON/hair_colours'; //Contains data about hair Colours.
+
+/* Male JSON files */
+import { male_beards } from './JSON/Male/male_beards'; //Contains data about Beards.
+import { male_clothes } from './JSON/Male/male_clothes'; //Contains data about Shirts & Pants.
+import { male_faces } from './JSON/Male/male_faces'; //Contains data about Faces.
+import { male_features } from './JSON/Male/male_features'; //Contains data about Features.
+import { male_gloves } from './JSON/Male/male_gloves'; //Contains data about Gloves for Vault Boy.
+import { male_hair } from './JSON/Male/male_hair'; //Contains data about Hairstyles.
+import { male_hands } from './JSON/Male/male_hands'; //Contains data about Hands. (No gloves.)
+import { male_headwear } from './JSON/Male/male_headwear'; //Contains data about Headwear.
+
+/* Female JSON files */
+import { female_clothes } from './JSON/Female/female_clothes'; //Contains data about Shirts & Pants.
+import { female_faces } from './JSON/Female/female_faces'; //Contains data about Faces.
+import { female_features } from './JSON/Female/female_features'; //Contains data about Features.
+import { female_gloves } from './JSON/Female/female_gloves'; //Contains data about Gloves for Vault Boy.
+import { female_hair } from './JSON/Female/female_hair'; //Contains data about Hairstyles.
+import { female_hands } from './JSON/Female/female_hands'; //Contains data about Hands. (No gloves.)
+import { female_headwear } from './JSON/Female/female_headwear'; //Contains data about Headwear.
+
+
+
+
+
+export {
+	all_races,
+	genders,
+	hair_colours,
+
+	male_beards,
+	male_clothes,
+	male_faces,
+	male_features,
+	male_gloves,
+	male_hair,
+	male_hands,
+	male_headwear,
+
+	female_clothes,
+	female_faces,
+	female_features,
+	female_gloves,
+	female_hair,
+	female_hands,
+	female_headwear,
+}
