51c20a65698170a1bf52dba9bde51ee0dfa9743a
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-04-10 22:39:04 -0400

    All styles JSON.
    A JSON with all styles, for mapping all buttons to the UI.

---
+++ src/JSON/all_styles.js
@@ -0,0 +1,30 @@
+export const all_styles = [ //An array of each 
+  {
+    id: 'shirt',
+    name: 'Shirt',
+    length: 'clothes.length',
+    current: 'current_shirt',
+    mode: 1 //Always visible, in Basic & Advanced.
+  },
+  {
+    id: 'pants',
+    name: 'Pants',
+    length: 'clothes.length',
+    current: 'current_pants',
+    mode: 1
+  },
+  {
+    id: 'hair',
+    name: 'Hair',
+    length: 'hair.length',
+    current: 'current_hair',
+    mode: 1
+  },
+  {
+    id: 'hair_colour',
+    name: 'Hair Colour',
+    length: 'hair_colours.length',
+    current: 'current_hair_colour',
+    mode: 0 //Only visible in Advanced.
+  },
+]
