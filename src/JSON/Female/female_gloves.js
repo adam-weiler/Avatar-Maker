63ca79d105b67f323edf48f9253c010b26132d25
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-04-16 23:07:00 -0400

    Updating the JSON files.

--- src/JSON/Female/female_gloves.js
+++ src/JSON/Female/female_gloves.js
@@ -1,10 +1,63 @@
-export const female_gloves = [ //An array of each Female Glove's ID, Atlas file, and name. There are ??.
+export const female_gloves = [ //An array of each Female Glove's ID and name. There are 8 pair.
   {
     id: 'glove0', //Append either vb_frontarm_hand or vb_backarm_hand to find class.
+    // atlas: 'atlas18', //Atlas is not needed. All Gloves are in atlas18.
     name: 'No gloves' //Show name in textbox.
   },
   {
-    id: 'glove3',
-    name: 'Something metal?'
+    id: 'glove25',
+    name: 'Metal armor' //Metal armor.
   },
+  {
+    id: 'glove2',
+    name: 'Metal armor - closed' //Metal armor.
+  },
+  {
+    id: 'glove3',
+    name: 'Durable gloves' //Minuteman uniform, Radiation suit, Wasteland gear.
+  },
+  {
+    id: 'glove7',
+    name: 'Durable gloves - closed' //Minuteman uniform, Radiation suit, Wasteland gear.
+  },
+  {
+    id: 'glove6',
+    name: 'BoS uniform' //BoS uniform, Advanced BoS uniform, Expert BoS uniform.
+  },
+    {
+    id: 'glove1',
+    name: 'BoS uniform - closed' //BoS uniform, Advanced BoS uniform, Expert BoS uniform.
+  },
+  {
+    id: 'glove9',
+    name: 'Power armor gloves' //T-60 power armor, X-01 Mk VI power armor, T-45 power armor, T-51 power armor.
+  },
+  {
+    id: 'glove11',
+    name: 'Power armor gloves - closed' //T-60 power armor, X-01 Mk VI power armor, T-45 power armor, T-51 power armor.
+  },
+  {
+    id: 'glove16',
+    name: 'Piper\'s gloves' //Piper's outfit.
+  },
+  {
+    id: 'glove18',
+    name: 'Piper\'s gloves - closed' //Piper's outfit.
+  },
+  {
+    id: 'glove17',
+    name: 'Boxing gloves'
+  },
+  {
+    id: 'glove19',
+    name: 'Baseball gloves'
+  },
+  {
+    id: 'glove23',
+    name: 'Phalanges gloves' //Skeleton costume.
+  },
+  {
+    id: 'glove24',
+    name: 'Phalanges gloves - closed' //Skeleton costume.
+  }
 ]
--- src/JSON/Male/male_gloves.js
+++ src/JSON/Male/male_gloves.js
@@ -1,69 +1,70 @@
-export const male_gloves = [ //An array of each Male Glove's ID, Atlas file, and name. There are 8.
+export const male_gloves = [ //An array of each Male Glove's ID and name. There are 8 pair.
   {
     id: 'glove0', //Append either vb_frontarm_hand or vb_backarm_hand to find class.
-    atlas: 'atlas18', //Append to atlas to find class.
+    // atlas: 'atlas18', //Atlas is not needed. All Gloves are in atlas18.
     name: 'No gloves' //Show name in textbox.
   },
   {
     id: 'glove3',
-    atlas: 'atlas18',
-    name: 'Something metal?'
+    name: 'Metal armor' //Metal armor.
+  },
+  {
+    id: 'glove6',
+    name: 'Metal armor - closed' //Metal armor.
   },
   {
     id: 'glove5',
-    atlas: 'atlas18',
-    name: 'BoS uniform'
+    name: 'BoS uniform' //BoS uniform, Advanced BoS uniform, Expert BoS uniform.
+  },
+  {
+    id: 'glove14',
+    name: 'BoS uniform - closed' //BoS uniform, Advanced BoS uniform, Expert BoS uniform.
   },
   {
     id: 'glove8',
-    atlas: 'atlas18',
-    name: 'Durable gloves' /* Radiation suit, Minuteman uniform. */
+    name: 'Durable gloves' //Minuteman uniform, Radiation suit, Wasteland gear.
+  },
+  {
+    id: 'glove10',
+    name: 'Durable gloves - closed' //Minuteman uniform, Radiation suit, Wasteland gear.
   },
   {
     id: 'glove9',
-    atlas: 'atlas18',
     name: 'Boxing gloves'
   },
   {
     id: 'glove11',
-    atlas: 'atlas18',
     name: 'Baseball gloves'
   },
   {
     id: 'glove12',
-    atlas: 'atlas18',
-    name: 'Power armor gloves'
+    name: 'Power armor gloves' //T-60 power armor, X-01 Mk VI power armor, T-45 power armor, T-51 power armor.
+  },
+  {
+    id: 'glove1',
+    name: 'Power armor gloves - closed' //T-60 power armor, X-01 Mk VI power armor, T-45 power armor, T-51 power armor.
   },
   {
     id: 'glove13',
-    atlas: 'atlas18',
-    name: 'Phalanges gloves' /* Skeleton costume. */
+    name: 'Phalanges gloves' //Skeleton costume.
+  },
+  {
+    id: 'glove7',
+    name: 'Phalanges gloves - closed' //Skeleton costume.
+  },
+
+  {
+    id: 'glovex',
+    name: '??? - Open?'
+  },
+  {
+    id: 'glovey',
+    name: '??? - Closed?' // 1 2 3 6 7 9 16 17 19
   },
 
   /* Closed gloves */
-  // {
-  //   id: 'glove1',
-  //   atlas: 'atlas18',
-  //   name: '???'
-  // },
-  // {
-  //   id: 'glove6',
-  //   atlas: 'atlas18',
-  //   name: '???'
-  // },
-  // {
-  //   id: 'glove7',
-  //   atlas: 'atlas18',
-  //   name: '???'
-  // },
-  // {
-  //   id: 'glove10',
-  //   atlas: 'atlas18',
-  //   name: '???'
-  // },
-  // {
-  //   id: 'glove14',
-  //   atlas: 'atlas18',
-  //   name: '???'
-  // }
+  
+  
+  
+
 ]
