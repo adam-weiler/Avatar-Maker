48248eeab099e7ffed41fadef2c02293d90584af
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-04-19 23:51:12 -0400

    Adding last Female Headwear.

--- src/JSON/Female/female_headwear.js
+++ src/JSON/Female/female_headwear.js
@@ -14,6 +14,7 @@ export const female_headwear = [ //An array of each Female Headwear ID, Atlas, a
     id: 'headwear2',
     atlas: 'atlas16',
     name: 'Cat ears',
+    disable_hair: 1
   },
   {
     id: 'headwear3',
@@ -104,32 +105,32 @@ export const female_headwear = [ //An array of each Female Headwear ID, Atlas, a
     name: 'Party hat'
   },
   {
-    id: 'headwear?',
-    atlas: 'atlas?',
+    id: 'headwear27',
+    atlas: 'atlas17',
     name: 'T-45 helmet', /* T-45 power armor */
     disable_hair: 1,
     disable_beard: 1,
     disable_feature: 1
   },
   {
-    id: 'headwear?',
-    atlas: 'atlas?',
+    id: 'headwear20',
+    atlas: 'atlas17',
     name: 'T-51 helmet', /* T-51 power armor */
     disable_hair: 1,
     disable_beard: 1,
     disable_feature: 1
   },
   {
-    id: 'headwear?',
-    atlas: 'atlas?',
+    id: 'headwear19',
+    atlas: 'atlas17',
     name: 'T-60 helmet', /* T-60 power armor */
     disable_hair: 1,
     disable_beard: 1,
     disable_feature: 1
   },
   {
-    id: 'headwear?',
-    atlas: 'atlas?',
+    id: 'headwear18',
+    atlas: 'atlas17',
     name: 'X-01 Mk VI helmet', /* X-01 Mk VI power armor */
     disable_hair: 1,
     disable_beard: 1,
@@ -163,32 +164,30 @@ export const female_headwear = [ //An array of each Female Headwear ID, Atlas, a
     id: 'headwear25',
     atlas: 'atlas17',
     name: 'Silver Shroud hat' //Alternatively Fedora, Formal Hat, or Trilby.
-},
-   {
+  },
+  {
     id: 'headwear26',
     atlas: 'atlas17',
     name: 'Santa hat' //Santa suit
   },
-
   {
-    id: 'headwear27',
+    id: 'headwear28',
     atlas: 'atlas17',
     name: 'Pirate hat and eyepatch'
   },
-
-    {
-    id: 'headwear28',
+  {
+    id: 'headwear29',
     atlas: 'atlas17',
     name: 'Press cap' //Piper's outfit
   },
- {
-    id: 'headwear28',
+  {
+    id: 'headwear30',
     atlas: 'atlas17',
-    name: 'Lady\'s bonnet' //Pilgrim outfit
+    name: 'Lady\'s bonnet', //Pilgrim outfit
+    disable_hair: 1
   },
-
-    {
-    id: 'headwear29',
+  {
+    id: 'headwear31',
     atlas: 'atlas17',
     name: 'Synth field helmet', /* Synth armor */
     disable_hair: 1,
@@ -201,6 +200,7 @@ export const female_headwear = [ //An array of each Female Headwear ID, Atlas, a
 
 
 
+ /* Headwear that's available in-game from Barbershop. */
 // Hard Hat
 // Rose hat
 // Laurels
@@ -217,3 +217,28 @@ export const female_headwear = [ //An array of each Female Headwear ID, Atlas, a
 // Aviator cap
 // Mascot hat
 // Party hat
+
+
+/* Headwear that's available from outfits or are locked. */
+//Metal helmet
+//T45, T51, T60, X01
+//Ghost costume
+//Sailor Hat
+//Baseball Cap
+//Elf hat
+//Santa Hat
+//Pilgrim bonnet
+//Skeleton Mask
+//Swimsuit & Goggles
+//Synth helmet
+//Shower Cap
+//Surgery bandages
+
+
+
+
+
+
+
+
+
