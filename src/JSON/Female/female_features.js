bd6006ecec71b98b6ef818fe8627cbe12e0756bf
Adam <42845085+adam-weiler@users.noreply.github.com>
2019-04-20 23:43:51 -0400

    Adding Female Features.

--- src/JSON/Female/female_features.js
+++ src/JSON/Female/female_features.js
@@ -1,15 +1,173 @@
 export const female_features = [ //An array of each Femle Facial Feature's ID, Atlas, and name. There are ??.
   {
-    id: 'mask0', 
-    atlas: '',
+    id: '', //ID name.
+    atlas: '', //Atlas file.
     name: 'No facial feature' //Show name in textbox.
+  },
+  {
+    id: 'feature1',
+    atlas: 'atlas01',
+    name: 'Blush makeup',
+    layer: 'Low' //1 is Lowest, 2 Medium, 3 is Highest.
+  },
+  {
+    id: 'feature16',
+    atlas: 'atlas01',
+    name: 'Green tears',
+    layer: 'Low' //???
+  },
+  {
+    id: 'feature15',
+    atlas: 'atlas01',
+    name: 'Freckles',
+    layer: 'Low' //???
+  },
+  {
+    id: 'feature14',
+    atlas: 'atlas01',
+    name: 'Red warpaint',
+    layer: 'Low' //???
+  },
+  {
+    id: 'feature13',
+    atlas: 'atlas01',
+    name: 'Beauty mark',
+    layer: 'Low' //???
+  },
+  {
+    id: 'feature12',
+    atlas: 'atlas01',
+    name: 'Lashes on fleek',
+    layer: 'Low' //???
+  },
+  {
+    id: 'feature2',
+    atlas: 'atlas01',
+    name: 'Titania makeup', //Fairy makeup
+    layer: 'Low'
+  },
+  {
+    id: 'feature18',
+    atlas: 'atlas01',
+    name: 'Lone Ranger', //Thief mask
+    layer: 'High'
+  },
+  {
+    id: 'feature20',
+    atlas: 'atlas01',
+    name: 'Skull makeup',
+    layer: 'Low'
+  },
+  {
+    id: 'feature7',
+    atlas: 'atlas01',
+    name: 'Raider makeup',
+    layer: 'Low'
+  },
+  {
+    id: 'feature9',
+    atlas: 'atlas01',
+    name: 'Cheek & Forehead scars',
+    layer: 'Low'
+  },
+  {
+    id: 'feature17',
+    atlas: 'atlas01',
+    name: 'Wrinkles',
+    layer: 'Low'
+  },
+  {
+    id: 'feature6',
+    atlas: 'atlas01',
+    name: 'Ghoulish', //Ghoul face
+    layer: 'Med'
+  },
+  {
+    id: 'feature5',
+    atlas: 'atlas01',
+    name: 'Cat Eyeglasses', //Big pink glasses
+    layer: 'Low' //???
+  },
+  {
+    id: 'feature4',
+    atlas: 'atlas01',
+    name: 'Reading Glasses',
+    layer: 'Low' //???
+  },
+  {
+    id: 'feature3',
+    atlas: 'atlas01',
+    name: 'Eyeglasses', //???
+    layer: 'Low' //???
+  },
+  {
+    id: 'feature10',
+    atlas: 'atlas01',
+    name: 'Pince-nez goggles', //Round Goggles
+    layer: 'High'
   },
   {
     id: 'feature11',
     atlas: 'atlas01',
-    name: '???' /* Fairy makeup */
+    name: 'Monocle',
+    layer: 'Med'
+  },  
+  
+  
+
+
+
+
+  {
+    id: 'feature8',
+    atlas: 'atlas01',
+    name: 'Clown nose',
+    layer: 'High'
+  },
+  {
+    id: 'feature19',
+    atlas: 'atlas01',
+    name: 'Surgical mask',
+    layer: 'Low' //???
   },
 
-
-  
 ]
+
+
+// Blush makeup
+// Green tears
+// Freckles
+// Red warpaint
+// Beauty mark
+// Eyeshadow
+// Titania
+// Bandit guy
+// White skull paint
+// Raider black paint
+// Scar on cheek
+// Wrinkles
+// Ghoul face
+// Pink glasses
+// Smaller pink glasses
+// Normal glasses
+// Goggles
+// Monocle
+// Comedian nose
+// Clown nose
+// Medical mask
+// Engineer hat
+// Rose hat
+// Caesar laurels
+// Soldier cap
+// Piper's hat
+// Camo Helmet
+// Brown helmet
+// Ninja mask
+// Radiation helmet
+// Wasteland gear
+// Cat ears
+// Pirate hat
+// Suave hat
+// Aviator goggles
+// Mascot head
+// Party hat
--- src/JSON/Male/male_features.js
+++ src/JSON/Male/male_features.js
@@ -8,123 +8,123 @@ export const male_features = [ //An array of each Male Facial Feature's ID, Atla
     id: 'feature11',
     atlas: 'atlas01',
     name: 'Oberon makeup', //Fairy makeup
-    layer: "Low" //1 is Lowest, 2 Medium, 3 is Highest.
+    layer: 'Low' //1 is Lowest, 2 Medium, 3 is Highest.
   },
   {
     id: 'feature6',
     atlas: 'atlas01',
     name: 'Lone Ranger', //Thief mask
-    layer: "High"
+    layer: 'High'
   },
   {
     id: 'feature7',
     atlas: 'atlas01',
-    name: 'Skull makeup', //Skull makeup
-    layer: "Low"
+    name: 'Skull makeup',
+    layer: 'Low'
   },
   {
     id: 'feature10',
     atlas: 'atlas01',
-    name: 'Raider makeup', //Raider black makeup
-    layer: "Low"
+    name: 'Raider makeup',
+    layer: 'Low'
   },
   {
     id: 'feature1',
     atlas: 'atlas01',
-    name: 'Cheek & Forehead scars', //Cheek Forehead scars
-    layer: "Low"
+    name: 'Cheek & Forehead scars',
+    layer: 'Low'
   },
   {
     id: 'feature9',
     atlas: 'atlas01',
     name: 'Tyrion scar', //Tyrion scar
-    layer: "Low"
+    layer: 'Low'
   },
   {
     id: 'feature8',
     atlas: 'atlas01',
     name: 'Nick Fury scar', //Nick Fury scar
-    layer: "Low"
+    layer: 'Low'
   },
   {
     id: 'feature5',
     atlas: 'atlas01',
     name: 'Wrinkles', //Wrinkles
-    layer: "Low"
+    layer: 'Low'
   },
   {
     id: 'feature15',
     atlas: 'atlas01',
     name: 'Ghoulish', //Ghoul face
-    layer: "Med"
+    layer: 'Med'
   },
   {
     id: 'feature14',
     atlas: 'atlas01',
     name: 'Nuka-bottle glasses', //Big round glasses
-    layer: "High"
+    layer: 'High'
   },
   {
     id: 'feature13',
     atlas: 'atlas01',
-    name: 'Glasses', //Regular glasses
-    layer: "High"
+    name: 'Reading Glasses', //Regular glasses
+    layer: 'High'
   },
   // {
   //   id: 'feature12',
   //   atlas: 'atlas01',
   //   name: 'Glasses with Van Dyke beard', Regular Glasses with Chin Beard
-  //   layer: "High"
+  //   layer: 'High'
   // },
   {
     id: 'feature4',
     atlas: 'atlas01',
     name: 'Road goggles', //Square Goggles
-    layer: "High"
+    layer: 'High'
   },
   {
     id: 'feature2',
     atlas: 'atlas01',
     name: 'Pince-nez goggles', //Round Goggles
-    layer: "High"
+    layer: 'High'
   },
   {
     id: 'feature3',
     atlas: 'atlas01',
     name: 'Monocle', //Monocle
-    layer: "Med"
+    layer: 'Med'
   },
   {
     id: 'feature18',
     atlas: 'atlas17',
     name: 'Nose glasses', //Comedian outfit
-    layer: "High"
+    layer: 'High'
   },
   {
     id: 'feature16',
     atlas: 'atlas01',
     name: 'Clown nose', //Clown nose
-    layer: "High"
+    layer: 'High'
   },
   {
     id: 'feature21',
     atlas: 'atlas17',
     name: 'Vulcan ears', //Sci-fi fan outfit
-    layer: "High"
+    layer: 'High'
   },
   {
     id: 'feature19',
     atlas: 'atlas17',
     name: 'Hockey mask', //Horror fan outfit
     disable_beard: 1,
-    layer: "High"
+    layer: 'High'
   },
   {
     id: 'feature20',
     atlas: 'atlas17',
     name: 'Luchador mask', //Wrestler outfit
     disable_hair: 1,
-    layer: "High"
+    layer: 'High'
   },
   {
     id: 'feature17',
@@ -132,6 +132,6 @@ export const male_features = [ //An array of each Male Facial Feature's ID, Atla
     name: 'Ninja mask', //Ninja outfit
     disable_hair: 1,
     disable_beard: 1,
-    layer: "High"
+    layer: 'High'
   }
 ]
